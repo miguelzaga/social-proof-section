896c3ad72bb54f299abfa3aeb5d302e5f109e6be
Miguel <miguezaga2@hotmail.com>
2022-10-02 19:07:34 -0500

    added html tags

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div>
-      <h1>New App</h1>
-      10,000+ of our users love our products. We only provide great products
-      combined with excellent customer service. See what our satisfied customers
-      are saying about our services. Rated 5 Stars in Reviews Rated 5 Stars in
-      Report Guru Rated 5 Stars in BestTech Colton Smith Verified Buyer "We
-      needed the same printed design as the one we had ordered a week prior. Not
-      only did they find the original order, but we also received it in time.
-      Excellent!" Irene Roberts Verified Buyer "Customer service is always
-      excellent and very quick turn around. Completely delighted with the
-      simplicity of the purchase and the speed of delivery." Anne Wallace
-      Verified Buyer "Put an order with this company and can only praise them
-      for the very high standard. Will definitely use them again and recommend
-      them to everyone!"
-    </div>
+    <main>
+      <h1>10,000+ of our users love our products.</h1>
+      <p>
+        We only provide great products combined with excellent customer service.
+        See what our satisfied customers are saying about our services.
+      </p>
+      <ul>
+        <li>Rated 5 Stars in Reviews</li>
+        <li>Rated 5 Stars in Report Guru</li>
+        <li>Rated 5 Stars in BestTech</li>
+      </ul>
+      <ul>
+        <li>
+          {" "}
+          Colton Smith Verified Buyer "We needed the same printed design as the
+          one we had ordered a week prior. Not only did they find the original
+          order, but we also received it in time. Excellent!"{" "}
+        </li>
+        <li>
+          {" "}
+          Irene Roberts Verified Buyer "Customer service is always excellent and
+          very quick turn around. Completely delighted with the simplicity of
+          the purchase and the speed of delivery."{" "}
+        </li>
+        <li>
+          Anne Wallace Verified Buyer "Put an order with this company and can
+          only praise them for the very high standard. Will definitely use them
+          again and recommend them to everyone!"{" "}
+        </li>
+      </ul>
+    </main>
   );
 }
 
